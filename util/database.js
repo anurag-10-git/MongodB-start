ff4ba4a175916e17941f48767899bfc68ddf86d3
anurag-10-git <anuragmehra2011@gmail.com>
2022-12-19 15:49:26 +0530

    streak broke

--- util/database.js
+++ util/database.js
@@ -1,14 +1,31 @@
- const mongodb = require('mongodb');
- const MongoClient = mongodb.MongoClient;
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
+let _db;
 
- const mongoConnect =(callback) => {
-    MongoClient.connect('mongodb+srv://anurag:R3DfiPAZnt78pkg8@clusters.nmumkwm.mongodb.net/?retryWrites=true&w=majority').then(client =>{
-  console.log('Connected');
-  callback(client);
- }).catch(err => {
-    console.log(err);
- });
-} 
-    
-module.exports = mongoConnect;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://anurag:R3DfiPAZnt78pkg8@clusters.nmumkwm.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("connect");
+      _db = client.db(); //this will connect to the shop
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const getDb = () => {
+   if(_db) {
+      return _db;
+   }
+   throw 'No database found';
+}
+
+exports.mongoConnect = mongoConnect; //connecting and storing to the database.This will keep on running
+
+exports.getDb = getDb; //this give access to the database if it exists
+
+//mongodb will take care using connection pooling ,it will take care of simultaneous interaction with database 
